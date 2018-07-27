8f119ff487e5a6f0a21b4ca1b25ce15e331cd227
parezp1 <34338440+parezp1@users.noreply.github.com>
2018-07-27 12:53:02 +0200

    Add files via upload

---
+++ install_python_old.ps1
@@ -0,0 +1,42 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
+{
+$certCallback=@"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            if(ServicePointManager.ServerCertificateValidationCallback ==null)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += 
+                    delegate
+                    (
+                        Object obj, 
+                        X509Certificate certificate, 
+                        X509Chain chain, 
+                        SslPolicyErrors errors
+                    )
+                    {
+                        return true;
+                    };
+            }
+        }
+    }
+"@
+    Add-Type $certCallback
+ }
+[ServerCertificateValidationCallback]::Ignore();
+
+
+
+$src = "https://bootstrap.pypa.io/get-pip.py"
+$dest = "$env:temp\get-pip.py"
+Invoke-WebRequest -Uri $src -OutFile $dest  -Proxy 'http://phdproxy.cezdata.corp:8080'
+python get-pip.py --no-index --find-links=$env:temp
+
+
+C:\python27\python.exe -m pip --no-index --find-links=$env:temp
---
+++ python_install.ps1
@@ -0,0 +1,41 @@
+﻿netsh winhttp import proxy source=ie
+
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
+{
+$certCallback=@"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            if(ServicePointManager.ServerCertificateValidationCallback ==null)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += 
+                    delegate
+                    (
+                        Object obj, 
+                        X509Certificate certificate, 
+                        X509Chain chain, 
+                        SslPolicyErrors errors
+                    )
+                    {
+                        return true;
+                    };
+            }
+        }
+    }
+"@
+    Add-Type $certCallback
+ }
+[ServerCertificateValidationCallback]::Ignore();
+
+
+$src = "http://www.python.org/ftp/python/3.3.5/python-3.3.5.amd64.msi"
+$dest = "$env:temp\python-3.3.5.amd64.msi"
+Invoke-WebRequest –Uri $src –OutFile $dest
+Unblock-File $dest
+Start-Process $dest
